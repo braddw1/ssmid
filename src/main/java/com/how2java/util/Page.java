6f2bfb247aa4b0fba5b8771c7c92d50b6d25c2ba
DW <appleddw@163.com>
2020-06-21 23:30:56 +0800

    初始化仓库

--- src/main/java/com/how2java/util/Page.java
+++ src/main/java/com/how2java/util/Page.java
@@ -25,7 +25,7 @@ public class Page {
 	}
 	
 	public void caculateLast(int total) {
-	    // 假设总数是50，是能够被5整除的，那么最后一页的开始就是45
+	    // 假设总数是50，是能够被5整除的，那么最后一页的开始就是45=====
 	    if (0 == total % count)
 	        last = total - count;
 	    // 假设总数是51，不能够被5整除的，那么最后一页的开始就是50
